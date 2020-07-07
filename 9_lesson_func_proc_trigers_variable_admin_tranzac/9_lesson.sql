cac5d5bb2c2bf8f205b796948852b9e52ab1eb22
oshkuk22 <oshkuk@yandex.ru>
2020-07-07 15:12:59 +0300

    9_lesson

---
+++ 9_lesson_func_proc_trigers_variable_admin_tranzac/9_lesson.sql
@@ -0,0 +1,88 @@
+ --  Transactions 1 task
+SHOW DATABASES;
+USE from_homework;
+SHOW tables;
+
+START TRANSACTION;
+  INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+  DELETE FROM shop.users WHERE id = 1;
+COMMIT;
+
+ --  Transactions 2 task
+ CREATE OR REPLACE VIEW products_catalogs AS
+SELECT
+  p.name AS product,
+  c.name AS catalog
+FROM
+  products AS p
+JOIN
+  catalogs AS c
+ON
+  p.catalog_id = c.id;
+ 
+ -- Functions 1 TASK
+USE vk;
+
+DROP FUNCTION IF EXISTS hello;
+
+DELIMITER //
+
+CREATE FUNCTION hello ()
+RETURNS TINYTEXT NO SQL
+BEGIN
+  DECLARE hour INT;
+  SET hour = HOUR(NOW());
+  CASE
+    WHEN hour BETWEEN 0 AND 5 THEN
+      RETURN "Доброй ночи";
+    WHEN hour BETWEEN 6 AND 11 THEN
+      RETURN "Доброе утро";
+    WHEN hour BETWEEN 12 AND 17 THEN
+      RETURN "Добрый день";
+    WHEN hour BETWEEN 18 AND 23 THEN
+      RETURN "Добрый вечер";
+  END CASE;
+END//
+
+DELIMITER ;
+SELECT NOW(), hello ();
+
+ -- Functions 2 TASK
+
+USE from_homework;
+SHOW tables;
+ 
+DELIMITER //
+
+CREATE TRIGGER validate_name_description_insert BEFORE INSERT ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'description are NULL';
+  END IF;
+END//
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, NULL, 9360.00, 2)//
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Материнская плата', 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 9360.00, 2)//
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 9360.00, 2)//
+
+CREATE TRIGGER validate_name_description_update BEFORE UPDATE ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'description are NULL';
+  END IF;
+END//
+
+
